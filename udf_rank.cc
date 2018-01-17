02e990cd3bdb26b7a915c4b7e1452ca13e8ad8b7
Michiel van Slobbe <michiel.van.slobbe+github@gmail.com>
2018-01-17 09:27:20 +0800

    updated comments

--- udf_rank.cc
+++ udf_rank.cc
@@ -38,7 +38,7 @@ SOFTWARE.
   SELECT integer, RANK(integer) FROM FOO ->
   1 0 // 0 successive '1' ; counter starts at 0
   1 1 // 1 successive '1' ; counter gets incremented because 1 == 1
-  2 1 // 0 successive '2' ; counter gets reset because 2 != 1
+  2 0 // 0 successive '2' ; counter gets reset because 2 != 1
 
   registering the function:
   CREATE AGGREGATE FUNCTION rank RETURNS STRING SONAME 'udf_rank.so';
